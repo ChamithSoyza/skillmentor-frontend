77fe8af9fb2c887348a53ea311471b0535b52e41
ChamithS_HO <madhumalcs@gmail.com>
2025-08-11 14:17:05 +0530

    Removed bootstrap clearly

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
