7da5c6c640bde3fd49238b3d1898eff734034d63
Chamith <madhumalcs@gmail.com>
2025-08-31 23:00:08 +0530

    Session management related developments made

--- src/pages/SessionManagement.tsx
+++ src/pages/SessionManagement.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Button} from "@/components/ui/button.tsx";
+import {useAuth} from "@clerk/clerk-react";
+import {BACKEND_URL} from "@/config/env.ts";
 
 const SessionManagement = () => {
+    const {getToken} = useAuth();
+    const [sessionData, setSessionData] = useState({});
+
+    // Fetch for session details
+    const getSessionDetails = async () => {
+        const token = await getToken({template: "skillmentor-auth-frontend"});
+        if (!token) return;
+
+        const required = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            redirect: "follow"
+        };
+
+        try {
+            const response = await fetch(`${BACKEND_URL}/academic/session`, required);
+            const data = await response.json();
+            setSessionData(data);
+        } catch (error) {
+            console.error("Error Fetching Session", error);
+        }
+    }
+
+    useEffect(() => {
+        getSessionDetails();
+    }, []);
+
+
     return (
         <>
             <div className="max-w-7xl mx-auto p-6">
@@ -27,7 +60,7 @@ const SessionManagement = () => {
                         <td className="px-4 py-2">Pending</td>
                         <td className="px-4 py-2">2025/05/08</td>
                         <td className="flex px-4 py-2 gap-2">
-                            <Button className="button  rounded bg-gray-600 text-white hover:bg-gray-500" >Cancel</Button>
+                            <Button className="button  rounded bg-gray-600 text-white hover:bg-gray-500">Cancel</Button>
                             <Button className="button rounded hover:bg-amber-500">Approve</Button>
                         </td>
                     </tr>
